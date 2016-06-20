7e5667ebd2c27e101fe53d6f3c340c5e90ac56d9
John Magee <jemagee@cox.net>
2016-06-20 13:02:43 -0700

    Tweaking the keg factory - need to set up the sequence to handle more randoms - not a perfect fix but workable for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :kegs
   resources :flavors
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/factories/kegs.rb
+++ spec/factories/kegs.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
 	factory :keg do
-		sequence(:serial_number) { |n| "16-23451#{n}B" }
+		sequence(:serial_number) { |n| "16-2345#{n+10}B" }
 	end
 end

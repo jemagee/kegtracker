48e404669f66a08e4aae794b55504337a5337743
John Magee <jemagee@cox.net>
2016-07-26 19:12:22 -0700

    Refactoring the keg factory to have more flexibiility in creating keg factories

--- spec/factories/kegs.rb
+++ spec/factories/kegs.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
 	factory :keg do
-		sequence(:serial_number, 10) { |n| "16-2345#{n}B" }
+		#sequence(:serial_number, 10) { |n| "16-2345#{n}B" }
+    serial_number {keg_number}
 	end
 end
+
+  def keg_number
+    x = (100000..999999).to_a.shuffle[0]
+    "16-#{x}B"
+  end
